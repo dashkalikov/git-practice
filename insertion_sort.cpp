9c85a8ae28dab89f68d3b03ea43459b45540378d
dashkalikov <dashkalikov@yandex.ru>
2025-10-05 20:36:06 +0300

    Добавлен счётчик количества сравнений

--- insertion_sort.cpp
+++ insertion_sort.cpp
@@ -5,15 +5,19 @@ using namespace std;
 
 void insertionSort(vector<int>& arr) 
 {
+    int comparisons = 0;  // счётчик сравнений
     for (size_t i = 1; i < arr.size(); ++i) 
     {
         int key = arr[i];
         int j = i - 1;
         while (j >= 0 && arr[j] > key) 
         {
+            comparisons++;
             arr[j + 1] = arr[j];
             j--;
         }
+        if (j >= 0) comparisons++;  // последнее сравнение, когда цикл прерывается
         arr[j + 1] = key;
     }
+    return comparisons;
 }
