c36700399251e17209c4cb0f62a0d28c0c483954
Shkalikov Denis <dashkalikov@yandex.ru>
2025-10-05 18:34:14 +0300

    Добавлены 3 файла с алгоритмами сортировки

---
+++ insertion_sort.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+
+void insertionSort(vector<int>& arr) 
+{
+    for (size_t i = 1; i < arr.size(); ++i) 
+    {
+        int key = arr[i];
+        int j = i - 1;
+        while (j >= 0 && arr[j] > key) 
+        {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j + 1] = key;
+    }
+}
